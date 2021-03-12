c47f6d8dab58fc5f5d3af8879a8e95c52dd7b76d
Scott O'Neil <scottieoneil@gmail.com>
2021-03-12 10:20:21 -0800

    add client create / details, controller

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -19,5 +19,23 @@ namespace Salon.Controllers
       List<Client> model = _db.Clients.Include(client => client.Stylist).ToList();
       return View(model);
     }
+    public ActionResult Create()
+    {
+      ViewBag.CategoryId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(item => item.ClientId == id);
+      return View(thisClient);
+    }
   }
 }
