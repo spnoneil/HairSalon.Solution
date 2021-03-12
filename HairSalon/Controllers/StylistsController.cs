f2ca794efaac73fd7aad9b9fdb096f1708ec5cd8
Scott O'Neil <scottieoneil@gmail.com>
2021-03-12 10:50:03 -0800

    add stylist create / details, controller

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -29,5 +29,11 @@ namespace Salon.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Details(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
   }
 }
