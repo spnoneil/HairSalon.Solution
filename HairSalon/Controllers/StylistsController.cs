80f1a906ca6e94e3681c14f206aa1792e5c5ea2c
Scott O'Neil <scottieoneil@gmail.com>
2021-03-12 09:47:01 -0800

    add stylist class / controller, simple layout

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Salon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Salon.Controllers
+{
+  public class StylistsController : Controller
+  {
+    private readonly SalonContext _db;
+    public StylistsController(SalonContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
+  }
+}
