ab23d7dbc6a9c8eb38f0b2438698bf7e3cb38ec1
Scott O'Neil <scottieoneil@gmail.com>
2021-03-12 09:26:57 -0800

    add home controller / index

---
+++ HairSalon/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Salon.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
